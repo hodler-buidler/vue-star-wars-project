473bb2682acede2e1e71fafb6b8034429715ca82
Nikita <30903256+NikitosGolubev@users.noreply.github.com>
2020-04-30 16:17:42 +0300

    Enhanced error handling, which allows to displays error-fact with toasts.

--- src/api/http.js
+++ src/api/http.js
@@ -1,16 +1,33 @@
 import axios from 'axios';
+import Vue from 'vue';
+import Toasted from 'vue-toasted';
+
+Vue.use(Toasted);
 
 const proxyToEscapeCors = 'https://cors-anywhere.herokuapp.com/';
 
 var instance = function createAxiosInstance({
     baseUrl = 'https://swapi.dev/api/'
 } = {}) {
-    return axios.create({
+    var instance = axios.create({
         baseURL: proxyToEscapeCors+baseUrl,
         headers: {
             'X-Requested-With': 'XMLHttpRequest'
-        }, 
+        }
     });
+
+    instance.interceptors.response.use(function(response) {
+        return response;
+      }, function (error) {
+
+        Vue.toasted.error('Oops.. Seems like network request failed :(, try again/reload the page.', {
+            duration: 5000
+        });
+
+        return Promise.reject(error);
+    }); 
+
+    return instance;
 };
 
 export default instance;
